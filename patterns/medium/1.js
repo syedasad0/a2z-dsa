d8c76c02be0c12d9d03ac5b3c2310db244705017
asus <syedasad0@gmail.com>
2024-04-09 00:10:03 +0530

    started practicing medium level pattern questions

---
+++ patterns/medium/1.js
@@ -0,0 +1,28 @@
+/*
+
+            *
+          * * *
+        * * * * *
+      * * * * * * *
+    * * * * * * * * *
+*/
+
+/*
+line 1 -> 1 star at 5th pos
+line 2 -> 2 stars at 4th and 5th pos
+line 3
+line 4
+line 5 -> 5 stars at 1st, 2nd, 3rd, 4th and 5th pos
+
+*/
+let rows = 5;
+for(let i=1; i<=rows; i++){
+    let row = "";
+    for(let j=1; j<=rows-i; j++){
+        row+= "  ";
+    }
+    for(let k=1; k<= 2*i-1; k++){
+        row+= "* ";
+    }
+    console.log(row);
+}
