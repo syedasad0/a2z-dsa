4cd07a377755474c52279761ef87b79ca544a57a
Mohammad Asad <syedasad0@gmail.com>
2024-04-30 05:23:22 +0530

    hashing started

--- hashing/basics/count_element.js
+++ hashing/basics/count_element.js
@@ -2,7 +2,7 @@
 
 //USING BRUTEFORCE O(N)
 
-function countOccurance(arr, num){
+/*function countOccurance(arr, num){
     let count =0;
     for(let i=0; i<arr.length; i++){
         if(arr[i] == num){
@@ -14,4 +14,19 @@ function countOccurance(arr, num){
 }
 let arr = [1,2,1,2,3,4,4,3,5,2,1];
 let num=4;
+console.log(countOccurance(arr, num));*/
+
+function countOccurance(arr, num){
+    // const maxSize = 1000000;
+    const maxSize = 15;;
+
+    let hashArray = new Array(maxSize).fill(0);
+    for(let i=0; i<arr.length; i++){
+        hashArray[arr[i]] = hashArray[arr[i]] + 1;
+    }
+    return hashArray[num];
+
+}
+let arr = [1,2,1,2,3,4,4,3,5,2,1];
+let num=6;
 console.log(countOccurance(arr, num));
