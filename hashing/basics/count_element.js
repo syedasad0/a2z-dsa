2f3294b58f69b5ee2d83f1b6468f51a90dbb935d
Mohammad Asad <syedasad0@gmail.com>
2024-05-02 05:14:47 +0530

    character hashing

--- hashing/basics/count_element.js
+++ hashing/basics/count_element.js
@@ -16,6 +16,8 @@ let arr = [1,2,1,2,3,4,4,3,5,2,1];
 let num=4;
 console.log(countOccurance(arr, num));*/
 
+//USING HASH ARRAY
+
 function countOccurance(arr, num){
     // const maxSize = 1000000;
     const maxSize = 15;;
@@ -30,3 +32,44 @@ function countOccurance(arr, num){
 let arr = [1,2,1,2,3,4,4,3,5,2,1];
 let num=6;
 console.log(countOccurance(arr, num));
+
+//CHARCTER HASHING FOR LOWERCASE ONLY
+// chaHash = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+//str = ['a', 's', 'a', 'd']
+/*
+function countCharacters(str, char) {
+    const maxSize = 26; // Assuming ASCII characters
+
+    let hashArray = new Array(maxSize).fill(0);
+    for (let i = 0; i < str.length; i++) {
+        console.log(`str.charCodeAt(i): ${str.charCodeAt(i)} :::::: ''a'.charCodeAt(0)': ${'a'.charCodeAt(0)}`);
+        const index = str.charCodeAt(i) - 'a'.charCodeAt(0);
+        console.log(`index: ${index}`);
+        hashArray[index]++;
+        console.log(`hashArray: ${hashArray}`);
+    }
+    return hashArray[char.charCodeAt(char) - 'a'.charCodeAt(0)]
+}
+
+let str = "asad";
+let char = 'a';
+console.log(countCharacters(str, char));
+*/
+//FOR LOWERCASE AND UPPERCASE BOTH
+function countCharacters(str, char) {
+    const maxSize = 256; // Assuming ASCII characters
+
+    let hashArray = new Array(maxSize).fill(0);
+    for (let i = 0; i < str.length; i++) {
+        console.log(`str.charCodeAt(i): ${str.charCodeAt(i)}`);
+        const index = str.charCodeAt(i) 
+        console.log(`index: ${index}`);
+        hashArray[index]++;
+        // console.log(`hashArray: ${hashArray}`);
+    }
+    return hashArray[char.charCodeAt(char)]
+}
+
+let str = "syedasad";
+let char = 'd';
+console.log(countCharacters(str, char));
