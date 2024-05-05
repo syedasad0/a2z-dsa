8847d80437868359e2e46ff0c8c4acd475efa034
asus <syedasad0@gmail.com>
2024-05-05 22:48:29 +0530

    started learning sorting techniques

---
+++ module_2/sorting_techniques/sorting_1/selection_sort.js
@@ -0,0 +1,6 @@
+//Given an array of N integers, write a program to implement the Selection sorting algorithm.
+function selectionSort(){
+
+}
+let arr = [13,46,24,52,20,9];
+console.log(selectionSort(arr))
