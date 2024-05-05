fce0357f6a4058b664d84f5dde6014ba5f62d840
asus <syedasad0@gmail.com>
2024-05-05 23:27:58 +0530

    selection sorting done

--- module_2/sorting_techniques/sorting_1/selection_sort.js
+++ module_2/sorting_techniques/sorting_1/selection_sort.js
@@ -1,6 +1,21 @@
 //Given an array of N integers, write a program to implement the Selection sorting algorithm.
-function selectionSort(){
-
+function selectionSort(arr) {
+    console.log('arr:', arr);
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minimumIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minimumIndex]) {
+                minimumIndex = j;
+            }
+        }
+        if (minimumIndex !== i) { //ensures that unnecessary swaps are avoided.
+            let temp = arr[i];
+            arr[i] = arr[minimumIndex];
+            arr[minimumIndex] = temp;
+        }
+    }
+    return arr;
 }
-let arr = [13,46,24,52,20,9];
-console.log(selectionSort(arr))
+
+let arr = [13, 46, 24, 52, 20, 9];
+console.log(selectionSort(arr));
