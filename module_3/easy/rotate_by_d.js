165e57be29cd15e987ca33011e2994e19f1e9511
asus <syedasad0@gmail.com>
2024-05-19 00:24:19 +0530

    rotate by d places problem solved

--- module_3/easy/rotate_by_d.js
+++ module_3/easy/rotate_by_d.js
@@ -12,7 +12,7 @@ function rotateByD(arr, d) {
         arr[i - d] = arr[i];
     }
     console.log('arr===>', arr);
-    let j=0;
+    let j = 0;
     for (let i = arr.length - d; i < arr.length; i++) {
         arr[i] = temp[j];
         j++;
@@ -24,3 +24,38 @@ let arr = [1, 2, 3, 4, 5];
 let d = 2
 //[3,4,5,1,2]
 console.log(rotateByD(arr, d));
+
+//Optimal Soulution [Complexity 2N]
+
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+function rotateLeft(arr, d) {
+    let n = arr.length;
+    d = d % n; // Handle cases where d > arr.length
+    if (d === 0) return arr;
+
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, n - 1);
+    reverse(arr, 0, n - 1);
+    //Use this for right rotation
+    /*
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, n - 1);
+    */
+    return arr;
+}
+
+let a = [1, 2, 3, 4, 5];
+let pos = 2;
+console.log(rotateLeft(a, pos)); // Output: [3, 4, 5, 1, 2]
+
+
