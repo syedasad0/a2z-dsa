b27ca3f1cfa03f2bf519669105862e23ee2567ac
asus <syedasad0@gmail.com>
2024-06-10 00:25:34 +0530

    practiced element in sorted array problem

--- interview_preparation/trajector/arrays/practice.js
+++ interview_preparation/trajector/arrays/practice.js
@@ -68,3 +68,35 @@ const arr2 = [1, 2, 5, 7, 9];
 const arr3 = [1, 3, 4, 5, 8]; //Output = [1,5]
 console.log(findCommon(arr1, arr2, arr3));
 */
+
+
+//WAP to find an element in rotated sorted array
+
+function findElement(arr, target){
+  let left = 0;
+  let right = arr.length - 1;
+  while(left <= right){
+    let mid = Math.floor(left + right)/2;
+    if(arr[mid] === target){
+      return mid;
+    }
+    if(arr[left] < arr[mid]){
+      if(arr[left] <= target && target < arr[mid]){
+        right = mid - 1;
+      }else{
+        left = mid + 1;
+      }
+    }else{
+      if(arr[mid] < target && target <= arr[right]){
+        left = mid + 1;
+      }else{
+        right = mid -1;
+      }
+    }
+  }
+  return -1
+
+}
+const nums = [4, 5, 6, 7, 0, 1, 2];
+const target = 7;
+console.log(findElement(nums, target))
