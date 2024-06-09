2c2e22554996af5ff281f09c8df7699aaa3b5bd2
asus <syedasad0@gmail.com>
2024-06-09 22:33:54 +0530

    practiced common in 3 arrays problem

--- interview_preparation/trajector/arrays/practice.js
+++ interview_preparation/trajector/arrays/practice.js
@@ -1,73 +1,70 @@
-// //next_greater_element
-
-// function nextGreaterElements(arr) {
-//     let stack = [];
-//     let nge = new Array(arr.length).fill(-1);
-//     for (let i = 2 * arr.length - 1; i >= 0; i--) {
-//         let currentElement = arr[i % arr.length];
-//         while (stack.length != 0 && stack[stack.length - 1] <= currentElement) {
-//             stack.pop();
-//         }
-//         if (i < arr.length) {
-//             if (stack.length > 0) {
-//                 nge[i] = stack[stack.length - 1];
-//             }
-//         }
-//         stack.push(currentElement)
-//     }
-//     return nge
-
-// }
-// const nums = [5, 7, 1, 2, 6, 0];
-// console.log(nextGreaterElements(nums))
-
-
-// //find element in rotated sorted array
-
-// function findElementInRotatedSortedArray(arr, target) {
-//     let left = 0;
-//     let right = arr.length - 1;
-
-//     while (left <= right) {
-//         let mid = Math.floor((left + right) / 2);
-//         if (arr[mid] == target) {
-//             return mid;
-//         }
-//         if (arr[left] <= arr[mid]) {
-//             if (arr[left] <= target && target < arr[mid]) {
-//                 right = mid - 1;
-//             }else{
-//                 left = mid + 1;
-//             }
-//         }else{
-//             if(arr[mid] <= target && target < arr[right]){
-//                 left = mid +1;
-//             }else{
-//                 right = mid -1;
-//             }
-//         }
-//     }
-//     return -1
-// }
-// const arr = [4, 5, 6, 7, 0, 1, 2];
-// const target = 0;
-// console.log(findElementInRotatedSortedArray(arr, target))
-
-// console.log('Start');
-// setImmediate(() => {
-//     console.log('Inside setImmediate');
-// });
-// console.log('End');
-Array.prototype.add = function(){
-  if(this.length == 0){
-    return 0;
+/*
+//find common in 3 sorted arrays
+//Brute
+function findCommon(arr1, arr2, arr3){
+  let commonAray = [];
+  for(let i=0; i<arr1.length; i++){
+    for(let j=0; j< arr2.length; j++){
+      if(arr1[i] == arr2[j]){
+        for(let k=0; k<arr3.length; k++){
+          if(arr3[k] == arr2[j]){
+            commonAray.push(arr3[k]);
+          }
+        }
+      }
+    }
   }
-  let result = this[0];
-  for(let i=1; i<this.length; i++){
-    result = result + this[i];
-  }
-  return result
+  return commonAray;
 }
 
-const arr = [2,3,4];
-console.log(arr.add());
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 5, 7, 9];
+const arr3 = [1, 3, 4, 5, 8]; //Output = [1,5]
+console.log(findCommon(arr1, arr2, arr3));
+//Better
+function findCommonUsingSets(arr1, arr2, arr3){
+  let set1 = new Set(arr1);
+  let set2 = new Set(arr2);
+  let set3 = new Set(arr3);
+  let commonArray = [];
+  set1.forEach(element => {
+    if(set2.has(element) && set3.has(element)){
+      commonArray.push(element);
+    }
+  })
+  return commonArray;
+}
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 5, 7, 9];
+const arr3 = [1, 3, 4, 5, 8]; //Output = [1,5]
+console.log(findCommonUsingSets(arr1, arr2, arr3));
+
+//Optimal
+function findCommon(arr1, arr2, arr3){
+  let i = 0; 
+  let j = 0;
+  let k = 0;
+  let commonAray = [];
+  while(i < arr1.length && j < arr2.length && k < arr3.length){
+    if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+      commonAray.push(arr1[i]);
+      i++;
+      j++;
+      k++;
+    }else if(arr1[i] < arr2[j]){
+      i++;
+    }else if(arr2[j] < arr3[k]){
+      j++;
+    }else{
+      k++;
+    }
+  }
+  return commonAray;
+
+}
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 5, 7, 9];
+const arr3 = [1, 3, 4, 5, 8]; //Output = [1,5]
+console.log(findCommon(arr1, arr2, arr3));
+*/
