349756d27204ac27d3a06b96d14d711998db5ed6
Syed Ahmed <syedasad0@gmail.com>
2024-08-12 22:30:52 +0530

    practices insertion sort

--- module_2/sorting_techniques/sorting_1/practice.js
+++ module_2/sorting_techniques/sorting_1/practice.js
@@ -12,23 +12,37 @@
 //     return arr;
 
 // }
-function selectionSort(arr){
-    for(i=0; i<arr.length-1; i++){
-        let minimum = i;
-        for(let j = i+1; j<arr.length; j++){
-            if(arr[j] < arr[minimum]){
-                minimum = j;
-            }
-        }
+// function selectionSort(arr){
+//     for(i=0; i<arr.length-1; i++){
+//         let minimum = i;
+//         for(let j = i+1; j<arr.length; j++){
+//             if(arr[j] < arr[minimum]){
+//                 minimum = j;
+//             }
+//         }
 
-        if(minimum !== i){
-            let temp = arr[minimum];
-            arr[minimum] = arr[i];
-            arr[i] = temp;
-         }
+//         if(minimum !== i){
+//             let temp = arr[minimum];
+//             arr[minimum] = arr[i];
+//             arr[i] = temp;
+//          }
+//     }
+//     return arr;
+// }
+
+
+function insertionSort(arr){
+    for(let i = 0; i<arr.length; i++){
+        let j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            let temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
     }
     return arr;
 }
 
-const arr = [10, 4, 5, 78, 9, 12, 13];
-console.log(selectionSort(arr));
+const arr = [23, 1, 10, 5, 2];
+console.log(insertionSort(arr));
