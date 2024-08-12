827c320f67ca0f70b3025a939131e0e855a56d97
Syed Ahmed <syedasad0@gmail.com>
2024-08-12 22:12:53 +0530

    practices selection sort algorithm

--- module_2/sorting_techniques/sorting_1/practice.js
+++ module_2/sorting_techniques/sorting_1/practice.js
@@ -1,18 +1,34 @@
-function bubbleSort(arr){
+// function bubbleSort(arr){
 
-    for(let i=0; i<arr.length; i++){
-        for(j=i+1; j<arr.length; j++){
-            if(arr[j]<arr[i]){
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+//     for(let i=0; i<arr.length; i++){
+//         for(j=i+1; j<arr.length; j++){
+//             if(arr[j]<arr[i]){
+//                 let temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+
+// }
+function selectionSort(arr){
+    for(i=0; i<arr.length-1; i++){
+        let minimum = i;
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[j] < arr[minimum]){
+                minimum = j;
             }
         }
+
+        if(minimum !== i){
+            let temp = arr[minimum];
+            arr[minimum] = arr[i];
+            arr[i] = temp;
+         }
     }
     return arr;
-
 }
 
-
-const arr = [10,4,5,78, 9,12,13];
-console.log(bubbleSort(arr));
+const arr = [10, 4, 5, 78, 9, 12, 13];
+console.log(selectionSort(arr));
